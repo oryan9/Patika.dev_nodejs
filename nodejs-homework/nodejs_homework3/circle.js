285800f5f5dba791a921436853200318b4d2f87a
ahmetoztrk <ahmetoztrk1709@gmail.com>
2022-02-22 18:09:45 +0300

    ADDED

---
+++ nodejs-homework/nodejs_homework3/circle.js
@@ -0,0 +1,9 @@
+function circleArea(r){
+    console.log("Circle Area : ", Math.PI * (r * r))
+}
+
+function circleCircumference(r) {
+    console.log("Circle Circumference : ", 2 * Math.PI * r)
+}
+
+module.exports = {circleArea, circleCircumference}
